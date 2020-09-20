a716ebc8787a78b5cf933c2e940e37a5ae9106d6
Ari Perkkio <ari.perkkio@gmail.com>
2020-09-20 17:03:04 +0300

    Replace console.log calls with direct process.stdout.write ones - Continuos console.clear + console.log tends to make terminals blink - Instead of updating whole log message, update only the changed characters

--- lib/process-logger.ts
+++ lib/process-logger.ts
@@ -1,28 +1,41 @@
-import chalk from 'chalk';
+import chalk, { Chalk } from 'chalk';
 
 import config from './config';
 
 interface Task {
     step: 'CLONE' | 'READ' | 'LINT';
+    color?: Chalk;
     repository: string;
     fileCount?: number;
     currentFileIndex?: number;
 }
 
+interface LogMessage {
+    content: string;
+    color?: Chalk;
+}
+
+interface LogUpdate {
+    character: string;
+    x: number;
+    y: number;
+}
+
 // Interval of how often logger should refresh termnial
 const REFRESH_INTERVAL_MS = 200;
 
+const DEFAULT_COLOR_METHOD = (c: string) => c;
 const TASK_TEMPLATE = (task: Task) => {
     switch (task.step) {
         case 'CLONE':
-            return `${chalk.yellow('[CLONING]')} ${task.repository}`;
+            return `[CLONING] ${task.repository}`;
 
         case 'READ':
-            return `${chalk.yellow('[READING]')} ${task.repository}`;
+            return `[READING] ${task.repository}`;
 
         case 'LINT':
             return (
-                `${chalk.green('[LINTING]')} ${task.repository} - ` +
+                `[LINTING] ${task.repository} - ` +
                 `${task.currentFileIndex}/${task.fileCount} files`
             );
 
@@ -30,6 +43,8 @@ const TASK_TEMPLATE = (task: Task) => {
             return `Unknown step ${task.step}`;
     }
 };
+const REPOSITORIES_STATUS_TEMPLATE = (scannedRepositories: number) =>
+    `Repositories (${scannedRepositories}/${config.repositories.length})`;
 
 /**
  * Logger for updating the terminal with current status
@@ -40,7 +55,7 @@ const TASK_TEMPLATE = (task: Task) => {
  */
 class ProcessLogger {
     /** Messages printed as a list under tasks */
-    messages: string[];
+    messages: LogMessage[];
 
     /** Messages of the task runners */
     tasks: Task[];
@@ -51,25 +66,34 @@ class ProcessLogger {
     /** Handle of refresh interval */
     intervalHandle: NodeJS.Timeout;
 
+    /** Contents of previous log. Used to diff new contents */
+    previousLog: string;
+
+    /** Colors of previous log */
+    previousColors: Chalk[];
+
     constructor() {
         this.messages = [];
         this.tasks = [];
         this.scannedRepositories = 0;
+        this.previousLog = '';
+        this.previousColors = [];
 
         this.intervalHandle = setInterval(() => {
             this.print();
         }, REFRESH_INTERVAL_MS);
+
+        console.clear();
     }
 
     /**
      * Clear refresh interval
      */
     onAllRepositoriesScanned() {
-        this.messages.push(
-            chalk.green(
-                `[DONE] Finished scan of ${this.scannedRepositories} repositories`
-            )
-        );
+        this.messages.push({
+            content: `[DONE] Finished scan of ${this.scannedRepositories} repositories`,
+            color: chalk.green,
+        });
 
         // Stop updating afer one final printing
         setTimeout(() => {
@@ -106,6 +130,7 @@ class ProcessLogger {
             fileCount,
             currentFileIndex: 0,
             step: 'LINT',
+            color: chalk.yellow,
         });
     }
 
@@ -122,46 +147,122 @@ class ProcessLogger {
             ? `with ${resultCount} errors`
             : 'without errors';
 
-        this.messages.push(color(`[DONE] ${repository} ${postfix}`));
+        this.messages.push({
+            content: `[DONE] ${repository} ${postfix}`,
+            color,
+        });
     }
 
     /**
      * Log end of a single file lint
      */
     onFileLintEnd(repository: string, currentFileIndex: number) {
-        this.updateTask(repository, { currentFileIndex, step: 'LINT' });
+        this.updateTask(repository, {
+            currentFileIndex,
+            step: 'LINT',
+            color: chalk.green,
+        });
     }
 
     /**
      * Log start of cloning of given repository
      */
     onRepositoryClone(repository: string) {
-        this.updateTask(repository, { step: 'CLONE' });
+        this.updateTask(repository, { step: 'CLONE', color: chalk.yellow });
     }
 
     /**
      * Log start of cloning of given repository
      */
     onRepositoryRead(repository: string) {
-        this.updateTask(repository, { step: 'READ' });
+        this.updateTask(repository, { step: 'READ', color: chalk.yellow });
     }
 
     /**
      * Print current tasks and messages
      */
     print() {
-        const formattedMessage = [
-            `Repositories (${this.scannedRepositories}/${config.repositories.length})`,
+        const rows = [
+            REPOSITORIES_STATUS_TEMPLATE(this.scannedRepositories),
             ...this.tasks.map(TASK_TEMPLATE),
             ' ', // Empty line between tasks and messages
-            ...this.messages,
-        ]
-            .filter(Boolean)
-            .join('\n');
+            ...this.messages.map(message => message.content),
+        ];
 
-        console.clear();
-        console.log(formattedMessage);
+        const colors = [
+            null,
+            ...this.tasks.map(task => task.color),
+            null,
+            ...this.messages.map(message => message.color),
+        ];
+
+        // Update colors of whole row
+        for (const [rowIndex, color] of colors.entries()) {
+            if (this.previousColors[rowIndex] !== color) {
+                const row = rows[rowIndex];
+                const colorMethod = color || DEFAULT_COLOR_METHOD;
+
+                process.stdout.cursorTo(0, rowIndex);
+                process.stdout.clearLine(0);
+                process.stdout.write(colorMethod(row));
+            }
+        }
+
+        const formattedLog = rows.join('\n');
+        const updates = diffLogs(this.previousLog, formattedLog);
+
+        // Update single character changes, e.g. step or file count changes
+        for (const { x, y, character } of updates) {
+            const colorMethod = colors[y] || DEFAULT_COLOR_METHOD;
+
+            // These were already updated by row's color update
+            if (this.previousColors[y] !== colors[y]) continue;
+
+            process.stdout.cursorTo(x, y);
+            process.stdout.write(colorMethod(character));
+        }
+
+        process.stdout.cursorTo(0, rows.length);
+        this.previousLog = formattedLog;
+        this.previousColors = colors;
     }
+}
+
+/**
+ * Compares two given strings and construct `LogUpdate` steps of the changes
+ * - This gets called during every print; optimization should be considered
+ */
+function diffLogs(previousLog: string, newLog: string): LogUpdate[] {
+    const updates: LogUpdate[] = [];
+    const previousRows = previousLog.split('\n');
+
+    for (const [y, row] of newLog.split('\n').entries()) {
+        const previousRow = previousRows[y];
+        const characters = row.split('');
+
+        if (!previousRow) {
+            for (const [x, character] of characters.entries()) {
+                updates.push({ character, x, y });
+            }
+            continue;
+        }
+
+        const previousCharacters = previousRow.split('');
+        for (const [x, character] of characters.entries()) {
+            if (character === previousCharacters[x]) continue;
+            updates.push({ x, y, character });
+        }
+
+        if (previousCharacters.length > characters.length) {
+            const rightPad = characters.length;
+
+            for (let i = 0; i < previousCharacters.length - rightPad; i++) {
+                updates.push({ x: rightPad + i, y, character: ' ' });
+            }
+        }
+    }
+
+    return updates;
 }
 
 export default new ProcessLogger();
