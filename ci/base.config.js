1e02166e859e802a6e4e09702ac146dd61a974eb
Ari Perkkio <ari.perkkio@gmail.com>
2020-12-15 20:26:45 +0200

    chore(repositories): add 14k repositories + shuffle for ci-runner

--- ci/base.config.js
+++ ci/base.config.js
@@ -1,8 +1,15 @@
 const repositories = require('../configs/repositories.json');
 const pathIgnorePattern = require('../configs/pathIgnorePattern');
 
+function shuffle(array) {
+    return array
+        .map(value => ({ value, order: Math.random() }))
+        .sort((a, b) => a.order - b.order)
+        .map(({ value }) => value);
+}
+
 module.exports = {
-    repositories: process.env.CI ? repositories : [repositories[0]],
+    repositories: process.env.CI ? shuffle(repositories) : [repositories[0]],
     extensions: ['js', 'jsx', 'ts', 'tsx'],
     pathIgnorePattern,
     rulesUnderTesting: [],
