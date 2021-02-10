7efa9d3559787f151fff0b2a7c6cb4a812c47bd5
Ari Perkkio <ari.perkkio@gmail.com>
2021-02-10 20:04:15 +0200

    test: log flaky integration tests output

--- test/integration/jest.setup.integration.ts
+++ test/integration/jest.setup.integration.ts
@@ -1,4 +1,10 @@
+import fs from 'fs';
+import { Jasmine } from 'jest-jasmine2/build/index.d';
+import { Reporter } from 'jest-jasmine2/build/types.d';
+
 import { clearResultsCache } from '../utils';
+
+declare const jasmine: Jasmine;
 
 // Extend timeout due to actual git clone
 jest.setTimeout(15000);
@@ -9,3 +15,15 @@ beforeEach(async () => {
     // Timeout between tests - otherwise constant `git clone` calls start failing
     await new Promise(r => setTimeout(r, 2000));
 });
+
+const specDone: Reporter['specDone'] = async result => {
+    if (result.status === 'failed') {
+        const debugLog = '/tmp/integration.test.debug.log';
+        if (!fs.existsSync(debugLog)) return console.error('Debug log missing');
+
+        const logContent = fs.readFileSync(debugLog, 'utf8');
+        console.log(`Debug log content of "${result.description}"`, logContent);
+    }
+};
+
+jasmine.getEnv().addReporter({ specDone });
