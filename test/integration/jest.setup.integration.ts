5bf4a80ad61495f209bf72184f5dfbd8fab252c6
Ari Perkkio <ari.perkkio@gmail.com>
2021-02-10 15:44:53 +0200

    test: add delay between integration tests

--- test/integration/jest.setup.integration.ts
+++ test/integration/jest.setup.integration.ts
@@ -3,6 +3,9 @@ import { clearResultsCache } from '../utils';
 // Extend timeout due to actual git clone
 jest.setTimeout(15000);
 
-beforeEach(() => {
+beforeEach(async () => {
     clearResultsCache();
+
+    // Timeout between tests - otherwise constant `git clone` calls start failing
+    await new Promise(r => setTimeout(r, 2000));
 });
