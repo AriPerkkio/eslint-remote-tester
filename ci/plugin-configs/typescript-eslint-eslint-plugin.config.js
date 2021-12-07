c4887fd964f2add954232a411270e06c83dbad49
Ari Perkkiö <ari.perkkio@gmail.com>
2021-12-07 18:31:08 +0000

    chore(ci-runner): disable multiple threads for ts-plugin

--- ci/plugin-configs/typescript-eslint-eslint-plugin.config.js
+++ ci/plugin-configs/typescript-eslint-eslint-plugin.config.js
@@ -6,6 +6,7 @@ const baseConfig = require('../base.config');
 
 module.exports = {
     ...baseConfig,
+    concurrentTasks: 1,
     eslintrc: function initializeLinter(options) {
         if (options) {
             const tsConfigLocation = findTsConfig(options);
